2448519668625f17c3d3704138876e774a218c32
Oleg Butuzov <butuzov@made.ua>
2021-05-30 12:48:58 +0300

    chores: version bumped to v0.3.3

--- Formula/deadlinks.rb
+++ Formula/deadlinks.rb
@@ -1,73 +1,68 @@
 class Deadlinks < Formula
-  include Language::Python::Virtualenv
+    include Language::Python::Virtualenv
 
-  desc "Health checks for your documentation links"
-  homepage "https://github.com/butuzov/deadlinks"
-  url "https://files.pythonhosted.org/packages/de/43/f4aa1e06c33a988b3d32d7faa9bf2c286966141d93978cfc64e576b8ec68/deadlinks-0.3.2.tar.gz"
-  sha256 "85d1cc8cf7ffad76fa64563d94ffe7ddeec9c99467171f13cbe4e5bc67d6dd56"
+    desc "Health checks for your documentation links"
+    homepage "https://github.com/butuzov/deadlinks"
+    url "https://files.pythonhosted.org/packages/c8/46/312cccc8715b2c4c21d643386e7e2419136242233e0396982bb28805229b/deadlinks-0.3.3.tar.gz"
+    sha256 "085fe08e6556a327047cba7df525a4be57ec515e149c9bce444ea9c6a638c702"
 
-  depends_on "python"
+    depends_on "python"
 
-  resource "requests" do
-    url "https://files.pythonhosted.org/packages/f5/4f/280162d4bd4d8aad241a21aecff7a6e46891b905a4341e7ab549ebaf7915/requests-2.23.0.tar.gz"
-    sha256 "b3f43d496c6daba4493e7c431722aeb7dbc6288f52a6e04e7b6023b0247817e6"
-  end
+    resource "requests" do
+      url "https://files.pythonhosted.org/packages/6b/47/c14abc08432ab22dc18b9892252efaf005ab44066de871e72a38d6af464b/requests-2.25.1.tar.gz"
+      sha256 "27973dd4a904a4f13b263a19c866c13b92a39ed1c964655f025f3f8d3d75b804"
+    end
 
-  resource "click" do
-    url "https://files.pythonhosted.org/packages/4e/ab/5d6bc3b697154018ef196f5b17d958fac3854e2efbc39ea07a284d4a6a9b/click-7.1.1.tar.gz"
-    sha256 "8a18b4ea89d8820c5d0c7da8a64b2c324b4dabb695804dbfea19b9be9d88c0cc"
-  end
+    resource "click" do
+      url "https://files.pythonhosted.org/packages/21/83/308a74ca1104fe1e3197d31693a7a2db67c2d4e668f20f43a2fca491f9f7/click-8.0.1.tar.gz"
+      sha256 "8c04c11192119b1ef78ea049e0a6f0463e4c48ef00a30160c704337586f3ad7a"
+    end
 
-  resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/09/06/3bc5b100fe7e878d3dee8f807a4febff1a40c213d2783e3246edde1f3419/urllib3-1.25.8.tar.gz"
-    sha256 "87716c2d2a7121198ebcb7ce7cccf6ce5e9ba539041cfbaeecfb641dc0bf6acc"
-  end
+    resource "urllib3" do
+      url "https://files.pythonhosted.org/packages/94/40/c396b5b212533716949a4d295f91a4c100d51ba95ea9e2d96b6b0517e5a5/urllib3-1.26.5.tar.gz"
+      sha256 "a7acd0977125325f516bda9735fa7142b909a8d01e8b2e4c8108d0984e6e0098"
+    end
 
-  resource "reppy" do
-    url "https://files.pythonhosted.org/packages/b0/b9/8bb8a4cd95dfc6038fb721fad95da8e9558ec936688150302cbd7874c45c/reppy-0.4.14.tar.gz"
-    sha256 "ad98ca17bfc39f543e81b85278f71ceaa1ba3f7c6817ff5a18f7305afa5fff87"
-  end
+    resource "six" do
+      url "https://files.pythonhosted.org/packages/71/39/171f1c67cd00715f190ba0b100d606d440a28c93c7714febeca8b79af85e/six-1.16.0.tar.gz"
+      sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
+    end
 
-  resource "six" do
-    url "https://files.pythonhosted.org/packages/21/9f/b251f7f8a76dec1d6651be194dfba8fb8d7781d10ab3987190de8391d08e/six-1.14.0.tar.gz"
-    sha256 "236bdbdce46e6e6a3d61a337c0f8b763ca1e8717c03b369e87a7ec7ce1319c0a"
-  end
+    resource "chardet" do
+      url "https://files.pythonhosted.org/packages/ee/2d/9cdc2b527e127b4c9db64b86647d567985940ac3698eeabc7ffaccb4ea61/chardet-4.0.0.tar.gz"
+      sha256 "0d6f53a15db4120f2b08c94f11e7d93d2c911ee118b6b30a04ec3ee8310179fa"
+    end
 
-  resource "chardet" do
-    url "https://files.pythonhosted.org/packages/fc/bb/a5768c230f9ddb03acc9ef3f0d4a3cf93462473795d18e9535498c8f929d/chardet-3.0.4.tar.gz"
-    sha256 "84ab92ed1c4d4f16916e05906b6b75a6c0fb5db821cc65e70cbd64a3e2a5eaae"
-  end
+    resource "certifi" do
+      url "https://files.pythonhosted.org/packages/06/a9/cd1fd8ee13f73a4d4f491ee219deeeae20afefa914dfb4c130cfc9dc397a/certifi-2020.12.5.tar.gz"
+      sha256 "1a4995114262bffbc2413b159f2a1a480c969de6e6eb13ee966d470af86af59c"
+    end
 
-  resource "certifi" do
-    url "https://files.pythonhosted.org/packages/41/bf/9d214a5af07debc6acf7f3f257265618f1db242a3f8e49a9b516f24523a6/certifi-2019.11.28.tar.gz"
-    sha256 "25b64c7da4cd7479594d035c08c2d809eb4aab3a26e5a990ea98cc450c320f1f"
-  end
+    resource "idna" do
+      url "https://files.pythonhosted.org/packages/cb/38/4c4d00ddfa48abe616d7e572e02a04273603db446975ab46bbcd36552005/idna-3.2.tar.gz"
+      sha256 "467fbad99067910785144ce333826c71fb0e63a425657295239737f7ecd125f3"
+    end
 
-  resource "idna" do
-    url "https://files.pythonhosted.org/packages/cb/19/57503b5de719ee45e83472f339f617b0c01ad75cba44aba1e4c97c2b0abd/idna-2.9.tar.gz"
-    sha256 "7588d1c14ae4c77d74036e8c22ff447b26d0fde8f007354fd48a7814db15b7cb"
-  end
+    def install
+      virtualenv_install_with_resources
+    end
 
-  def install
-    virtualenv_install_with_resources
-  end
+    test do
+      # version assertion
+      assert_match /#{version}/, shell_output("#{bin}/deadlinks --version")
 
-  test do
-    # version assertion
-    assert_match /#{version}/, shell_output("#{bin}/deadlinks --version")
+      # deaddomain expected result
+      (testpath/"localhost.localdomain.log").write <<~EOS
+        ===========================================================================
+        URL=<http://localhost.localdomain>; External Checks=Off; Threads=1; Retry=0
+        ===========================================================================
+        Links Total: 1; Found: 0; Not Found: 1; Ignored: 0; Redirects: 0
+        ---------------------------------------------------------------------------\e[?25h
+        [ failed ] http://localhost.localdomain
+      EOS
 
-    # deaddomain expected result
-    (testpath/"localhost.localdomain.log").write <<~EOS
-      ===========================================================================
-      URL=<http://localhost.localdomain>; External Checks=Off; Threads=1; Retry=0
-      ===========================================================================
-      Links Total: 1; Found: 0; Not Found: 1; Ignored: 0; Redirects: 0
-      ---------------------------------------------------------------------------\e[?25h
-      [ failed ] http://localhost.localdomain
-    EOS
-
-    # deaddomain assertion
-    output = shell_output("deadlinks localhost.localdomain --no-progress --no-colors")
-    assert_equal (testpath/"localhost.localdomain.log").read, output
-  end
+      # deaddomain assertion
+      output = shell_output("deadlinks localhost.localdomain --no-progress --no-colors")
+      assert_equal (testpath/"localhost.localdomain.log").read, output
+    end
 end
